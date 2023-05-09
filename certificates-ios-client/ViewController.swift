f80a88682ebab8e64443184ccd20dc6b17873f2e
Yevhenii Serdiukov <zenaserdykov2004@gmail.com>
2023-05-09 16:14:46 +0300

    Initial Commit

---
+++ certificates-ios-client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  certificates-ios-client
+//
+//  Created by Jonikorjk on 5/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ certificates-ios-clientUITests/certificates_ios_clientUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  certificates_ios_clientUITestsLaunchTests.swift
+//  certificates-ios-clientUITests
+//
+//  Created by Jonikorjk on 5/9/23.
+//
+
+import XCTest
+
+final class certificates_ios_clientUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
