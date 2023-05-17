2c52241b571a4e299504370becdeb58ffb6d7342
Yevhenii Serdiukov <zenaserdykov2004@gmail.com>
2023-05-17 15:01:05 +0300

    Deleted pods from repository

--- Pods/Target Support Files/Pods-certificates-ios-client-certificates-ios-clientUITests/Pods-certificates-ios-client-certificates-ios-clientUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_certificates_ios_client_certificates_ios_clientUITests : NSObject
-@end
-@implementation PodsDummy_Pods_certificates_ios_client_certificates_ios_clientUITests
-@end
