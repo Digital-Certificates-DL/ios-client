8f9716a3709c49ed3f227c93dd10a33fbd0839d4
Yevhenii Serdiukov <zenaserdykov2004@gmail.com>
2023-07-03 19:11:04 +0300

    Added 1 month between blocktime and certificate date

--- certificates-ios-client/Managers/Workers/CertificateVerifier.swift
+++ certificates-ios-client/Managers/Workers/CertificateVerifier.swift
@@ -49,28 +49,33 @@ class CertificateVerifier: CertificateVerifierProtocol {
         _ qrData: QrData
     ) async -> QrDataValidated {
         var qrDataValidated = QrDataValidated(qrData: qrData, date: "", certificateIsValid: false)
-        
         guard let isVerified = try? Secp256k1Manager().verifySignatureMagic(qrData.message, qrData.signature, qrData.address), isVerified else {
             return qrDataValidated
         }
-
         guard let certificate = await certificateProvider.getCertificate(with: qrDataValidated.signature) else {
             return qrDataValidated
         }
-        
+        if !isMessageInRightFormat(qrData.message) {
+            return qrDataValidated
+        }
         if certificate.txHash == "–" {
             qrDataValidated.certificateIsValid = true
             return qrDataValidated
         }
-        
         guard let transaction = await transactionProvider.provideTransactionWithId(txHash: certificate.txHash) else {
             return qrDataValidated
         }
+        let blockTime = transaction.status.blockTime
+        qrDataValidated.date = getDateString(timestamp: blockTime) ?? ""
         
-        let timestamp = transaction.status.blockTime
-        qrDataValidated.date = getDate(timestamp: timestamp) ?? ""
-
-        guard let timeStamping = getTimestampingField(transaction: transactionProvider.transaction) else {
+        guard let qrCodeDateString = getDateFromMessage(qrData.message),
+              let blockTime = transaction.status.blockTime else {
+            return qrDataValidated
+        }
+        if !isCertificateDataValid(qrCodeDateString: qrCodeDateString, blockTime: blockTime) {
+            return qrDataValidated
+        }
+        guard let timeStamping = getTimestampingField(transaction: transaction) else {
             return qrDataValidated
         }
         
@@ -83,7 +88,68 @@ class CertificateVerifier: CertificateVerifierProtocol {
 
 private extension CertificateVerifier {
     
-    func getDate(timestamp: Int?) -> String? {
+    func isCertificateDataValid(
+        qrCodeDateString: String,
+        blockTime: Int
+    ) -> Bool {
+        guard let qrTimeInterval = getTimeInterval(from: qrCodeDateString),
+              let blockTimeInterval = getTimeInterval(from: blockTime) else { return false }
+        let qrDate = Date(timeIntervalSince1970: qrTimeInterval)
+        let blockTime = Date(timeIntervalSince1970: blockTimeInterval)
+        return isDifferenceWithinOneMonth(date1: qrDate, date2: blockTime)
+    }
+    
+    func isDifferenceWithinOneMonth(date1: Date, date2: Date) -> Bool {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.month], from: date1, to: date2)
+
+        // Check if the difference is less than or equal to 1 month
+        if let monthDifference = components.month, monthDifference <= 1 {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    func getTimeInterval(from string: String, dateFormat: String = "dd.MM.yyyy") -> TimeInterval? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = dateFormat
+
+        if let date = dateFormatter.date(from: string) {
+            let timeInterval = date.timeIntervalSince1970
+            return timeInterval
+        } else {
+            return nil
+        }
+    }
+    
+    func getTimeInterval(from integer: Int) -> TimeInterval? {
+        return TimeInterval(integer)
+        
+    }
+
+    func getDateFromMessage(_ string: String) -> String? {
+        let strings = string.split(separator: " ")
+        let date = strings[0]
+        return String(date)
+    }
+    
+    func isMessageInRightFormat(_ string: String) -> Bool {
+        let pattern = #"^\d{2}\.\d{2}\.\d{4}\s.*"#
+
+        if let regex = try? NSRegularExpression(pattern: pattern, options: []) {
+            let range = NSRange(location: 0, length: string.count)
+            if let match = regex.firstMatch(in: string, options: [], range: range) {
+                return true
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+    }
+    
+    func getDateString(timestamp: Int?) -> String? {
         guard let timestamp = timestamp else { return nil }
         let date = Date(timeIntervalSince1970: Double(timestamp))
         let dayTimePeriodFormatter = DateFormatter()
@@ -92,6 +158,12 @@ private extension CertificateVerifier {
         return dateString
     }
      
+    func getDate(timestamp: Int?) -> Date? {
+        guard let timestamp = timestamp else { return nil }
+        let date = Date(timeIntervalSince1970: Double(timestamp))
+        return date
+    }
+    
     func getTimestampingField(transaction: Transaction?) -> String? {
         let outputs = transaction?.vout
         let opReturn = outputs?.first { output in
